9e0c59aa9a3d1f2cd6cfd4c42405bd1f4d11e0e5
ReneGRomCodes <renegromcodes@gmail.com>
2026-01-19 11:16:32 +0100

    added JS solution for 11-08-25.

--- javascript/08-25/11-08-25 Vowel Balance.js
+++ javascript/08-25/11-08-25 Vowel Balance.js
@@ -15,3 +15,37 @@ If there's an odd number of characters in the string, ignore the center characte
 6. is_balanced("abcdefghijklmnopqrstuvwxyz") should return False.
 7. is_balanced("123A#b!E&*456-o.U") should return True.
  */
+
+function isBalanced(s) {
+    s = s.toLowerCase();
+    const vowels = new Set(["a", "e", "i", "o", "u"]);
+    const len = s.length;
+
+    let center = Math.floor(len / 2);
+
+    // ignore center character if odd.
+    const left = s.slice(0, center);
+    const right = s.slice(len % 2 === 0 ? center : center + 1);
+
+    let countA = 0;
+    let countB = 0;
+
+    for (const ch of left) {
+        if (vowels.has(ch)) countA++;
+    }
+
+    for (const ch of right) {
+        if (vowels.has(ch)) countB++;
+    }
+
+    return countA === countB;
+}
+
+
+console.log(isBalanced("racecar"));
+console.log(isBalanced("Lorem Ipsum"));
+console.log(isBalanced("Kitty Ipsum"));
+console.log(isBalanced("string"));
+console.log(isBalanced(" "));
+console.log(isBalanced("abcdefghijklmnopqrstuvwxyz"));
+console.log(isBalanced("123A#b!E&*456-o.U"));
